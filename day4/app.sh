a7551b700abfbc3404a1535517e35b1373826fe0
Banti4750 <bantikumar6203818460@gmail.com>
2026-03-22 11:53:58 +0530

    last part

---
+++ day4/app.sh
@@ -0,0 +1,14 @@
+lsof -> list the open files and the processes that are using them, allowing you to identify which files are currently being accessed by which processes on the system.
+lsof -i :80 -> lsof command with the -i option for list the open files that are associated with network connections on port 80, allowing you to identify which processes are using
+
+netstat -> netstat command for display network connections, routing tables, and interface statistics, allowing you to monitor and troubleshoot network activity on your system.
+
+
+nslookup -> nslookup command for query the Domain Name System (DNS) to obtain information about domain names, IP addresses, and other DNS records, allowing you to troubleshoot DNS-related issues and gather information about network resources.
+nslookup google.com -> nslookup command for query the DNS to obtain information about the google.com domain, including its IP address and other DNS records, allowing you to troubleshoot DNS-related issues and gather
+
+if config -> ifconfig command for display and configure network interfaces on a Unix-like operating system, allowing you to view the current network configuration and make changes to the network settings.
+ifconfig eth0 -> ifconfig command for display the configuration of a specific network interface, in this case, the eth0 interface, allowing you to view the IP address, subnet mask, and
+
+
+cut -c 1-2 file.txt -> cut command with the -c option for extract specific characters from each line of a file, in this case, extracting the first two characters from each line of "file.txt" and displaying the output.
